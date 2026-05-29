8ed0a85512f3b07cdc26116641a4a24a74f25c65
tahmina-official <277821518+tahmina-official@users.noreply.github.com>
2026-05-28 21:09:58 -0400

    CACHE DOM ELEMENTS

---
+++ script.js
@@ -0,0 +1,33 @@
+// ======================================
+// CACHE DOM ELEMENTS
+// ======================================
+
+const plantTrackerForm =
+    document.getElementById("plantTrackerForm");
+
+const plantNameInput =
+    document.getElementById("plantNameInput");
+
+const sunlightSelect =
+    document.getElementById("sunlightSelect");
+
+const waterInput =
+    document.getElementById("waterInput");
+
+const validationMessage =
+    document.getElementById("validationMessage");
+
+const searchPlantInput =
+    document.getElementById("searchPlantInput");
+
+const filterButtons =
+    document.querySelectorAll(".filterButton");
+
+const themeToggleBtn =
+    document.getElementById("themeToggleBtn");
+
+const plantCardContainer =
+    document.querySelector(".plantCardContainer");
+
+const plantCounterText =
+    document.getElementById("plantCounterText");
